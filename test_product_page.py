909e5f7e3572c55a70ec71b6cd63d6eef19ed699
SergeyMihailovich <fsm_mail.ru>
2022-06-29 22:02:03 +0300

    обавлена проверка регистрации

--- test_product_page.py
+++ test_product_page.py
@@ -8,6 +8,7 @@ from .pages.main_page import MainPage
 from .pages.basket_page import BasketPage
 
 
+@pytest.mark.need_review
 @pytest.mark.login
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
@@ -19,6 +20,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.register_new_user(email, password)
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
         page = MainPage(browser, link)
@@ -69,7 +71,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
-@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -89,7 +91,7 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.should_be_massage_in_basket()
 
 
-@pytest.mark.skip
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/hacking-exposed-wireless_208/'
     page = BasketPage(browser, link)
@@ -99,7 +101,7 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.should_be_massage_in_basket()
 
 
-@pytest.mark.skip
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
